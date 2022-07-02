b27ea897655afc76a3f7d422650be5a97267f3eb
CoyoteColt <CoyoteColt@outlook.com>
2022-07-01 21:39:51 -0300

    fix remove comments

--- Teste3.py
+++ Teste3.py
@@ -4,6 +4,9 @@
 • O maior valor de faturamento ocorrido em um dia do mês;
 • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
 
+
+
+
 IMPORTANTE:
 a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
@@ -14,10 +17,10 @@ import pandas as pd
 arquivo = open('Faturamento Diário.json')
 dados = arquivo.read()
 faturamento_df = pd.read_json(dados)
-#display(faturamento_df)
+
 
 faturamento_df.drop([3, 4, 6, 10, 11, 17, 18, 24, 25], axis=0, inplace=True)
-#display(faturamento_df)
+
 
 print('O menor faturamento foi de: R${:,.2f}'.format(faturamento_df['valor'].min()))
 print('O maior faturamento foi de: R${:,.2f}'.format(faturamento_df['valor'].max()))
